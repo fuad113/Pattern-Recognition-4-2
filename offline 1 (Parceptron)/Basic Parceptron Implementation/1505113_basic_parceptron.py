6394a25978716477e5dc0e8899974a051a03a1f3
fuad113 <anfuad96@yahoo.com>
2020-09-25 13:29:38 +0600

    Basic parceptron implementation done

--- offline 1 (Parceptron)/Basic Parceptron Implementation/1505113_basic_parceptron.py
+++ offline 1 (Parceptron)/Basic Parceptron Implementation/1505113_basic_parceptron.py
@@ -1,36 +1,6 @@
 import numpy as np
 import math
 
-
-# creating object for each classtype
-class ClassObject:
-    def __init__(self, class_name,num_of_features):
-        self.class_name = class_name
-        self.num_of_features=num_of_features
-        self.features_array = []
-        self.mean_array=[]
-        self.sd_array=[]
-        self.covarmat=[]
-
-    def convertofloat(self):
-        for i in range(len(self.features_array)):
-            for j in range(no_of_features):
-                self.features_array[i][j]=float(self.features_array[i][j])
-
-
-    def printobject(self):
-        print(self.features_array)
-
-    def noofelementsinclass(self):
-        temp=len(self.features_array)
-        return temp
-
-
-
-# dictionary for maintaining classes
-classDictionary = {}
-# reading the file and getting the no. of features , classes and samples given
-
 f = open("./trainLinearlySeparable.txt", "r")
 first_line = f.readline()
 
@@ -45,52 +15,126 @@ no_of_samples = int(temp[2])
 lines = f.readlines()[0:]
 f.close()
 
-for i in range(no_of_samples):
-    lines_data = lines[i].split()
-    class_name = lines_data[no_of_features]
-
-    if class_name in classDictionary:
-        classDictionary[class_name].features_array.append(lines_data[:no_of_features])
-
-    else:
-        classDictionary[class_name] = ClassObject(class_name, no_of_features)
-        classDictionary[class_name].features_array.append(lines_data[:no_of_features])
-
-#converting string to float in dictionary
-for i in classDictionary:
-    ob= classDictionary[i]
-    ob.convertofloat()
-
-
-# matrix multiplication function
-def matrixmuldot(mat1=[],mat2=[]):
-    m1= np.array(mat1)
-    m2= np.array(mat2)
-    result=np.dot(m1,m2)
-    return result
-
-# transpose of a matrix
-def tranposemat(mat=[]):
-    m=np.array(mat)
-    result= m.transpose()
-    return result
-
 # training of the dataset
-#defining the value of rho
-rho=1
 
+print("Parceptron Trainning")
+
+#defining the value of rho
+rho=0.1
 #creating the W(t) matrix
 weightMatrix=[]
 
-for i in range(no_of_features):
+for i in range(no_of_features+1):
     weightMatrix.append(0.0)
 
+weightMatrix =np.array(weightMatrix)
+
+# np.random.seed(113)
+#
+# weightMatrix = np.random.uniform(-1, 1, no_of_features + 1)
+
+for i in range(1000):
+
+    #defining the misclassified feature vector set
+    misClassified = []
+
+    for j in range(no_of_samples):
+
+        lineData = lines[j].split()
+
+        className = lineData[no_of_features]
+
+        className =int(className)
+
+        #creating feature vector
+        featureVector= lineData[:no_of_features]
+
+        # append 1 in the end of the feature vector
+        featureVector.append(1.0)
+
+        #converting the values of feature vector to float
+        for k in range(no_of_features+1):
+            featureVector[k]= float(featureVector[k])
+
+        featureVector = np.array(featureVector)
+
+        dotValue = np.dot(weightMatrix, featureVector)
+
+        if(dotValue >= 0 ):
+            experimentalClass = 1
+        else:
+            experimentalClass = 2
+
+        #checking if misclassified and multiplying delX with feature vector
+        if(experimentalClass== 1 and className== 2):
+            delX= 1
+            featureVector = featureVector * delX
+            misClassified.append(featureVector)
+
+        elif(experimentalClass == 2 and className == 1):
+            delX = -1
+            featureVector = featureVector * delX
+            misClassified.append(featureVector)
+
+    #checking if the misclassified array is zero or not. If zero then converged. Break the loop
+    if(len(misClassified) == 0):
+        break
+
+    #updating the Weight vector
+    sumVector =[]
+    for k in range(no_of_features+1):
+        sumVector.append(0.0)
+
+    sumVector = np.array(sumVector)
+
+    for k in range(len(misClassified)):
+        sumVector+=misClassified[k]*rho
+
+    weightMatrix = weightMatrix - sumVector
 
 
+#testing of data set
+
+print("Parceptron Testing")
+
+f2 = open("./testLinearlySeparable.txt", "r")
+lines = f2.readlines()[0:]
+f2.close()
+
+correctClassifyCounter = 0
+
+for i in range(no_of_samples):
+    lineData = lines[i].split()
+
+    className = lineData[no_of_features]
+
+    xVector = lineData[:no_of_features]
+
+    xVector.append(1.0)
+
+    for k in range(no_of_features + 1):
+        xVector[k] = float(xVector[k])
+
+    xVector =np.array(xVector)
 
 
+    dotProdValue = np.dot(weightMatrix,xVector)
+
+    #checking the value of w*X. if w*x > 0 then class 1 otherwise class 2
+    if dotProdValue > 0:
+        predictedClass = 1
+    else:
+        predictedClass = 2
+
+    className =int(className)
+    predictedClass = int(predictedClass)
+
+    if (className == predictedClass) :
+        correctClassifyCounter = correctClassifyCounter + 1
 
 
+print("CorrectClassify: ", correctClassifyCounter)
 
+accuracy = (correctClassifyCounter / no_of_samples) * 100
 
-
+print("Accuracy: ",accuracy, "%")
