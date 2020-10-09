2e0d7f6d4d80cf58736220d737ddcbf2ff9ee7a3
fuad113 <anfuad96@yahoo.com>
2020-10-10 02:23:24 +0600

    Implementation of neural network done

--- offline 2 (Neural Network)/Implementation/1505113.py
+++ offline 2 (Neural Network)/Implementation/1505113.py
@@ -1,7 +1,11 @@
 import  numpy as np
-import random
-maximumIteration = 1
+import copy
+maximumIteration = 1000
 
+np.random.seed(1)
+
+#learning rate
+mu= 0.01
 
 #---------------------------------------------------------------------------------------
 #reading the trainning dataset
@@ -12,6 +16,9 @@ lines = f.readlines()
 f.close()
 
 numOfFeatures = len(lines[0].split()) - 1
+
+numOfsamples = len(lines)
+
 
 #Counter for counting number of classes
 numOfClass = 0
@@ -39,7 +46,13 @@ for line in lines:
 
 #converitng feature vector and class vector to numpy array.
 trainningX = np.array(featureVector).T
-trainningY = np.array(classVector)
+
+#Trainnig Y is kept for tracking which sample is classified as which class. its row= num of class + 1 (avoid O indexing)
+#its column = num of samples
+trainningY = np.zeros((numOfClass , numOfsamples))
+
+for i in range(numOfsamples):
+    trainningY[classVector[i]-1][i] =1
 
 #printing the class and feature informations
 print('No of features: ', numOfFeatures)
@@ -70,10 +83,10 @@ trainningX = (trainningX - mean)/sd
 
 #---------------------------------------------------------------------------------------
 #defining number of Hidden layers. Output layer is included in the hidden layer
-numOfHiddenLayers = 3
+numOfHiddenLayers = 5
 
 #defining the number of nodes in the hidden layers. Output layer number of nodes = number of class
-numOfNodesHiddenLayer = 3
+numOfNodesHiddenLayer = 5
 
 #creating an array to keep track which layer has how many nodes
 perLayerNodesNumber = []
@@ -107,7 +120,7 @@ for i in range(1, numOfHiddenLayers+1 , 1 ):
 
     for j in range(thisLayerNodeNumber):
         for k in range(previousLayerNodeNumber):
-            thisLayerWeightMatrix[j][k] = random.random()
+            thisLayerWeightMatrix[j][k] = np.random.randn()
 
     thisLayerWeightMatrix = np.array(thisLayerWeightMatrix)
 
@@ -119,22 +132,196 @@ weightMatrix = np.array(weightMatrix)
 
 
 #---------------------------------------------------------------------------------------
-#working of Forward Propagation and Backward Propagation
+#working of Forward Propagation
+
+#3d vectors/array to save the values of Y and V
+yDic = {}
+vDic = {}
 
 #sigmoid function. It returns a vector
 def sigmoid(vector):
     return (1 / (1 + np.exp(-vector)))
 
+#sigmoid differentiated function
+def sigmoidDifferentiated(vector):
+    return (sigmoid(vector) * (1 - sigmoid(vector)))
 
+def forwardPropagation(featureVec):
+    global yDic,vDic
+
+    y = np.array(featureVec)
+    yDic[str(0)] = y
+    for i in range(1, numOfHiddenLayers+1 , 1):
+        v = np.dot(weightMatrix[i] , y)
+        y= sigmoid(v)
+        yDic[str(i)] = y
+        vDic[str(i)] = v
+    return y
+#---------------------------------------------------------------------------------------
+
+
+
+#---------------------------------------------------------------------------------------
+#working of backword Propagation
+#calculation of delta rj
+def delRJCalculation(sampleNum, layerNum , previousDelRJ):
+
+    if(layerNum == numOfHiddenLayers):
+        value1 = yDic[str(layerNum)][:,sampleNum]
+        value2 = trainningY[:,sampleNum]
+
+        tempRJ = np.subtract(value1 , value2)
+
+        value3 = vDic[str(layerNum)][:,sampleNum]
+        fPrime = sigmoidDifferentiated(value3)
+
+        delRJ = np.multiply(tempRJ, fPrime)
+
+    else:
+        value1 = vDic[str(layerNum)][:,sampleNum]
+        fPrime = sigmoidDifferentiated(value1)
+
+        fPrimeShape = fPrime.shape
+        fPrime = fPrime.reshape(fPrimeShape[0] , 1)
+
+        value2 = weightMatrix[layerNum + 1]
+        value3 = np.diagflat(fPrime)
+        tempRJ = np.dot(value2 , value3)
+
+        value4 = previousDelRJ.T
+        value5 = tempRJ
+        delRJ = np.dot(value4,value5).T
+
+    return delRJ
+
+def backwardPropagation():
+    weightMatrixNew = copy.deepcopy(weightMatrix)
+
+    for i in range(numOfsamples):
+        delRJ = None
+        for j in range(numOfHiddenLayers , 0  , -1 ):
+            delRJ  = delRJCalculation(i , j , delRJ)
+            #reshape delRj to make it a column matrix
+            delRJShape = delRJ.shape
+            delRJ = delRJ.reshape(delRJShape[0] , 1)
+
+            #size of previous Y
+            prevYsize = len(yDic[str(j-1)][:,i])
+
+            #multiplication part and updating part
+            value1 = delRJ
+            value2 = yDic[str(j-1)][:,i].reshape(1, prevYsize)
+            dotProdValue = np.dot(value1, value2)
+
+            tempW= mu * dotProdValue
+
+            weightMatrixNew[j] = weightMatrixNew[j] - tempW
+
+    for i in range(1 , numOfHiddenLayers + 1 ,1):
+        weightMatrix[i] = weightMatrixNew[i]
+
+#---------------------------------------------------------------------------------------
+
+
+
+#---------------------------------------------------------------------------------------
+#trainning the dataset
+print("Trainning On Process")
 for i in range(maximumIteration):
+    yCap =forwardPropagation(trainningX)
+    backwardPropagation()
+#---------------------------------------------------------------------------------------
 
-    #working of forward propagation
-    Yvector = np.array(trainningX)
-    for j in range(1, numOfHiddenLayers+1 , 1):
-        mul = np.dot(weightMatrix[j] , Yvector )
-        Yvector = sigmoid(mul)
 
-    Ycap = np.array(Yvector)
+
+
+#---------------------------------------------------------------------------------------
+#reading the testing dataset
+f = open("./testNN.txt", "r")
+
+#reading the lines of the dataset
+lines = f.readlines()
+f.close()
+
+numOfsamplesTest = len(lines)
+
+#declaring vectors for Features and Classes
+featureVectorTest = []
+classVectorTest = []
+
+for line in lines:
+    lineData = line.split()
+
+    className = int(lineData[numOfFeatures])
+    classVectorTest.append(className)
+
+    tempVector =  (lineData[:numOfFeatures])
+
+    #converting the feature vales to flaot
+    for i in range(numOfFeatures):
+        tempVector[i] = float (tempVector[i])
+
+    featureVectorTest.append(tempVector)
+
+
+#converitng feature vector and class vector to numpy array.
+testingX = np.array(featureVectorTest).T
+testingY = copy.deepcopy(classVectorTest)
+#---------------------------------------------------------------------------------------
+
+
+
+#---------------------------------------------------------------------------------------
+#scaling the testing dataset
+
+#calculating the feature vectors mean and standart deviation first
+#axis=1 means the mean is calculated along the row of the feature matrix
+tempMean = testingX.mean(axis=1)
+#converting the mean row vector to a column vector
+mean = tempMean.reshape(numOfFeatures,1)
+
+#axis=1 means standard deviation is calculated along the row of the feature matrix
+tempSd = testingX.std(axis=1)
+#converting the standard deviation row vector to a column vector
+sd = tempSd.reshape(numOfFeatures,1)
+
+#normalizing rule :  (feature vector - mean vector)/ standard deviation vector
+testingX = (testingX - mean)/sd
+#---------------------------------------------------------------------------------------
+
+
+
+
+#---------------------------------------------------------------------------------------
+#testing work
+print("Testing On Process")
+correctClassified = 0
+
+yCap = forwardPropagation(testingX)
+
+for i in range(numOfsamplesTest):
+    min = yCap[0][i]
+    predictedClass = 1
+
+    for j in range(numOfFeatures):
+        if(yCap[j][i] > min ):
+            min =yCap[j][i]
+            predictedClass = j+1
+
+    actualClass = testingY[i]
+
+    if(actualClass == predictedClass):
+        correctClassified += 1
+
+
+accuracy = (correctClassified / numOfsamplesTest) * 100
+
+print('Accuracy: ' , accuracy ,'%')
+
+
+
+
+
 
 
 
