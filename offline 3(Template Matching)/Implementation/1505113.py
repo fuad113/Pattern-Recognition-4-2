377046c77cd9d5a658f9ebbcf99353f317662c7d
fuad113 <anfuad96@yahoo.com>
2020-11-04 12:40:05 +0600

    template matching 2D Log done

--- offline 3(Template Matching)/Implementation/1505113.py
+++ offline 3(Template Matching)/Implementation/1505113.py
@@ -10,7 +10,7 @@ M=shapeOfReferenceImage[0]
 N= shapeOfReferenceImage[1]
 
 #set the value of p
-p=3
+p=2
 
 #-----------------------------------------------------------------------------------
 #read the video
@@ -37,7 +37,7 @@ numOfFrames = len(frames)
 
 
 #-----------------------------------------------------------------------------------
-#Exhaustive search
+# #Exhaustive search
 startingTime = time.time()
 outputFrames =[]
 
@@ -174,6 +174,170 @@ print("Time Taken: ", timeDuration ,"seconds")
 print("Searching Done: " ,exhaustiveSearching)
 
 #-----------------------------------------------------------------------------------
+
+
+#-----------------------------------------------------------------------------------
+#2D logarithmic searching
+startingTime = time.time()
+outputFrames2Dlog =[]
+
+twoDLogSearching = 0
+
+#exhasutive working with the first frame of the video
+firstFrameShape= frames[0].shape
+
+I= firstFrameShape[0]
+J= firstFrameShape[1]
+
+prevI = -1
+PrevJ = -1
+
+#working of the 1st frame
+minD = np.inf
+markedIIndex = 0
+markedJIndex = 0
+
+iStart = 0
+iEnd = I-M+1
+jStart = 0
+jEnd = J-N+1
+
+frame = frames[0]
+
+for i in range(iStart, iEnd):
+    for j in range(jStart, jEnd):
+        # calculate the D(m,n)
+        # first make a sub matrix
+        subMatrix = frame[i:i + M, j:j + N]
+        subMatrix = subMatrix.astype(np.int64)
+
+        # calculation of difference
+        tempRef = referenceImage.astype(np.int64)
+        difference = tempRef - subMatrix
+        difference = np.absolute(difference)
+
+        diffSquare = difference * difference
+
+        d = np.sum(diffSquare)
+
+        twoDLogSearching += 1
+
+        if (d < minD):
+            minD = d
+            markedIIndex = i
+            markedJIndex = j
+
+# update the prevI and prevJ
+prevI = markedIIndex
+prevJ = markedJIndex
+
+# convert the frame to rgb
+rgbFrame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+# draw the rectangle around the reference image
+cv2.rectangle(rgbFrame, (markedJIndex, markedIIndex), (markedJIndex + N, markedIIndex + M), (0, 0, 255), 2)
+# append to the output frame
+outputFrames2Dlog.append(rgbFrame)
+
+#working with other frames
+
+for index in range(1 , numOfFrames , 1 ):
+
+    prevP = p
+    frame = frames[index]
+
+    while (True):
+        #determine the value of k and d
+        k = np.ceil( np.log2(prevP) )
+        distance = int ( np.power(2,k-1) )
+
+        #now determine the 9 points
+        points ={
+            "1": [prevI , prevJ ],
+            "2": [prevJ-distance ,prevJ ],
+            "3": [prevI-distance ,prevJ+distance ],
+            "4": [prevI-distance ,prevJ-distance ],
+            "5": [prevI+distance ,prevJ ],
+            "6": [prevI+distance ,prevJ+distance ],
+            "7": [prevI+distance ,prevJ-distance ],
+            "8": [prevI , prevJ+distance ],
+            "9": [prevI , prevJ-distance ]
+        }
+
+        #now work with these 9 points only
+        #find the point with the minimum distance
+
+        indexKey =0
+        minD = np.inf
+        twoDLogSearching+=1
+
+        for key in points:
+
+            i= points[key][0]
+            j= points[key][1]
+
+            if ( (i < 0) or (i > I - M) or (j < 0) or (j > J - N)):
+                continue
+
+            # calculate the D(m,n)
+            # first make a sub matrix
+            subMatrix = frame[i:i + M, j:j + N]
+            subMatrix = subMatrix.astype(np.int64)
+
+            #calculation of difference
+            tempRef = referenceImage.astype(np.int64)
+            difference = tempRef - subMatrix
+            difference = np.absolute(difference)
+
+            diffSquare = difference * difference
+
+            d = np.sum(diffSquare)
+
+            if(d < minD):
+                minD = d
+                indexKey =key
+
+        #point found. set the point as the prevI and prevJ
+        prevI = points[indexKey][0]
+        prevJ = points[indexKey][1]
+        prevP = prevP/2
+
+        #when the distance is 1, break the while loop
+        if (distance == 1):
+            break
+
+
+    # convert the frame to rgb
+    rgbFrame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+
+    # draw the rectangle around the reference image
+    cv2.rectangle(rgbFrame, (prevJ, prevI), (prevJ + N, prevI + M), (0, 0, 255), 2)
+    # append to the output frame
+    outputFrames2Dlog.append(rgbFrame)
+
+
+endingTime = time.time()
+
+timeDuration =endingTime -startingTime
+
+#now make the output video
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+output = cv2.VideoWriter("1505113_2DLog.mov", fourcc, fps, (J, I))
+
+for frame in outputFrames2Dlog:
+    output.write(frame)
+
+output.release()
+
+print("2D Log search")
+print("-----------------")
+print("Value of P: " , p)
+print("Time Taken: ", timeDuration ,"seconds")
+print("Searching Done: " ,twoDLogSearching)
+
+#-----------------------------------------------------------------------------------
+
+
+
 
 
 
